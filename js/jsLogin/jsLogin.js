151d65686ae25b920d5f644627f357e3f2c59856
DESKTOP-H39OAJ3\danielpumayauli <danielpumayauli@gmail.com>
2017-03-14 22:36:24 -0500

    First commit

---
+++ js/jsLogin/jsLogin.js
@@ -0,0 +1,33 @@
+(function() {
+
+  "use strict";
+
+    $(window).load(function() {
+        setTimeout(function() {
+          $("body").addClass('loaded');
+        }, 1000);
+    });
+    
+    $(document).on("ready",function(){
+        $("#btnIngresar").on("click",function(e){
+            e.preventDefault();
+            var usuario = ($("#txtUsuario").val()).trim();
+            var password = ($("#txtPassword").val()).trim();
+            $("#msgUsuario, #msgPassword").addClass("noVisible");
+            
+            if(usuario === ""){
+                $("#msgUsuario").removeClass("noVisible");
+            }
+            if(password === ""){
+                $("#msgPassword").removeClass("noVisible");
+            }
+            if(usuario !== "" && password !== ""){
+                $("#loginForm").submit();
+            }
+            
+        });
+    });
+  
+})();
+
+
